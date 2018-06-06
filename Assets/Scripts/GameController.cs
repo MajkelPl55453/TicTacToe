b447a20911e9eb074e8e659a9eea88794593bfd6
MAGDALENA/w57015 <w57015@student.wsiz.rzeszow.pl>
2018-06-06 18:08:57 +0200

    poprawa logiki wybierania wygranego oraz wyświetlania linii, dodanie zliczania punktów oraz wgranie packages
    Signed-off-by: MAGDALENA/w57015 <w57015@student.wsiz.rzeszow.pl>

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,7 +13,11 @@ public class GameController : MonoBehaviour {
     public int[] markedSpaces; // id pól klikniętych przez konkretnego gracza
     public Text winnerText; // Trzyma komponent tekstu dla wygranego gracza
     public GameObject[] winningLines; // przetrzymuje linie dla wygrania
-    public GameObject winnerPanel;
+    public GameObject winnerPanel; // panel zabezpieczający przed klikaniem przysików po wygraniu
+    public int xPlayerScore;
+    public int oPlayerScore;
+    public Text xPlayersScoreText;
+    public Text oPlayersScoreText;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +27,8 @@ public class GameController : MonoBehaviour {
     void GameSetup() {
         whoseTurn = 0;
         turnCounter = 0;
+        xPlayerScore = 0;
+        oPlayerScore = 0;
         turnIcons[0].SetActive(true);
         turnIcons[1].SetActive(false);
         for (int i = 0; i < tictactoeSpaces.Length; i++)
@@ -67,14 +73,14 @@ public class GameController : MonoBehaviour {
          * jeżeli gra się skończyła, to któraś z poniższych zmiennych będzie wynosiła 3 lub 6. Dlaczego? Ponieważ w marked space zapisaliśmy id użytkownika, 
          * a do wygrania potrzebujemy 3 pól, więc 3 * 1 = 3, a 3*2 = 6.
          */
-        int s1 = markedSpaces[0] + markedSpaces[1] + markedSpaces[3];
+        int s1 = markedSpaces[0] + markedSpaces[1] + markedSpaces[2];
         int s2 = markedSpaces[3] + markedSpaces[4] + markedSpaces[5];
         int s3 = markedSpaces[6] + markedSpaces[7] + markedSpaces[8];
         int s4 = markedSpaces[0] + markedSpaces[3] + markedSpaces[6];
         int s5 = markedSpaces[1] + markedSpaces[4] + markedSpaces[7];
         int s6 = markedSpaces[2] + markedSpaces[5] + markedSpaces[8];
         int s7 = markedSpaces[0] + markedSpaces[4] + markedSpaces[8];
-        int s8 = markedSpaces[1] + markedSpaces[4] + markedSpaces[6];
+        int s8 = markedSpaces[2] + markedSpaces[4] + markedSpaces[6];
 
         //Dodanie zmiennych do tablicy w celu sprawdzenia pętlą
         var solutions = new int[] { s1, s2, s3, s4, s5, s6, s7, s8 };
@@ -94,10 +100,14 @@ public class GameController : MonoBehaviour {
         winnerText.gameObject.SetActive(true);
         if (whoseTurn == 0)
         {
+            xPlayerScore++;
+            xPlayersScoreText.text = xPlayerScore.ToString();
             winnerText.text = "Gracz X wygrał!";
         }
         else if(whoseTurn == 1)
         {
+            oPlayerScore++;
+            oPlayersScoreText.text = oPlayerScore.ToString();
             winnerText.text = "Gracz O wygrał";
         }
         winningLines[indexIn].SetActive(true);
